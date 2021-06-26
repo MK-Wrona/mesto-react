b1950adb5672c8759d82e131770507907126a6f1
Mara Krukovskaya <krukovmargaery@yandex.ru>
2021-06-26 18:06:08 +0300

    fix: catch within initCards/2nd rev req

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const [selectedCard, setSelectedCard] = React.useState({});{/*загружала работу дважды, т.к. первый раз забыла добавить объект,возможно, Вы проверили первый вариант, или я неверно поняла замечание*/}
+  const [selectedCard, setSelectedCard] = React.useState({});
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true);
@@ -29,7 +29,7 @@ function App() {
     setIsEditAvatarPopupOpen(false);
     setIsEditProfilePopupOpen(false);
     setIsAddPlacePopupOpen(false);
-    setSelectedCard(false);
+    setSelectedCard({});
   }
 
   return (
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,7 +1,7 @@
 function ImagePopup({card, onClose}) {
 
     return (
-      <div className={`pop-up pop-up_full ${card ? 'pop-up_opened' : ''}`}>
+      <div className={`pop-up pop-up_full ${card.name ? 'pop-up_opened' : ''}`}>
         <div className="pop-up__box">
           <img src={card.link} alt="Полноэкранное изображение" className="pop-up__full-image" />
           <h3 className="pop-up__title-full-image">{card.name}</h3>
